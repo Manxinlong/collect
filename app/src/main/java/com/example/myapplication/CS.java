6247b458fd6e6971982d90febd61448ec01f4928
mxl <37889337+Manxinlong@users.noreply.github.com>
2023-05-31 21:32:14 +0800

    Add files via upload

--- app/src/main/java/com/example/myapplication/CS.java
+++ app/src/main/java/com/example/myapplication/CS.java
@@ -31,19 +31,20 @@ public class CS {
     private int mChannelConfig = AudioFormat.CHANNEL_IN_MONO;
     private int mAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
     private static final int BUFFER_SIZE = 4096; // 缓冲区大小
+    int windowSize = 1024; // 滑动窗口大小
 
-    private static final int WINDOW_SIZE = 1024; // 窗口大小
-    private static final int STEP_SIZE = 1; // 重叠大小
-//    private double[] overlapBuffer = new double[WINDOW_SIZE - STEP_SIZE]; // 重叠缓冲区
-
-    private static final int TARGET_FREQUENCY = 10000; // 目标频率
-    private static final int MAX_FREQUENCY_DEVIATION = 10; // 最大频率偏差
+    // 目标频率和允许的浮动范围
+    private double targetFrequency = 10000; // 10kHz
+    private double frequencyRange = 1000; // 上下浮动10Hz
 
     private boolean mIsRecording = false;
     public boolean mTargetFrequencyDetected = false; // 新增变量，用于判断是否检测到目标频率
     public static long RecordTime;
 
-
+    private long mStartTime = 0; // 当前数组读取时的时间
+    private long mPreviousStartTime = 0; // 上一次记录的时间
+    private boolean mIsFirstTime = true; // 标记是否第一次读取音频数据
+    private short[] mPreviousBuffer;
 
 
     @SuppressLint("MissingPermission")//忽略该警告
@@ -62,6 +63,23 @@ public class CS {
         mIsRecording = true;
 
         while (mIsRecording && !mTargetFrequencyDetected) {
+            // 保存上一次记录的时间
+            mPreviousStartTime = mStartTime;
+
+            // 读取音频数据前保存当前时间
+            long currentTime = System.currentTimeMillis();
+
+            // 第一次读取音频数据时，初始化 mStartTime
+            if (mIsFirstTime) {
+                mStartTime = currentTime;
+                mIsFirstTime = false;
+            }
+
+//            // 计算数组处理的时间间隔
+//            long deltaTime = currentTime - mStartTime;
+
+            // 更新 mStartTime
+            mStartTime = currentTime;
             // 读取音频数据
             int bytesRead = mAudioRecord.read(audioBuffer, 0, BUFFER_SIZE);
 
@@ -73,90 +91,167 @@ public class CS {
         mAudioRecord.stop();
         mAudioRecord.release();
     }
-    public void stopRecording() {
-        mIsRecording = false;
-    }
 
-    private void processAudioData(short[] audioData, int bytesRead) {
-        // 窗口处理循环
-        for (int i = 0; i < bytesRead; i += 1) {
-            if (i + WINDOW_SIZE <= bytesRead) {
-                // 提取窗口数据
-                double[] windowData = extractWindowData(audioData, i);
+    private void processAudioData(short[] audioBuffer, int bytesRead) {
+        // 对该数组内的数据进行目标检测
+        boolean targetDetected = detectTargetFrequency(audioBuffer, bytesRead);
 
+        if (targetDetected) {
+            // 若检测到目标频率，拼接之前保存的数组数据
+            short[] combinedBuffer = combineBuffers(mPreviousBuffer, audioBuffer, bytesRead);//4096*2
 
+            //转换为double类型
+            double[] combinedBuffer_double = convertToDoubleArray(combinedBuffer,combinedBuffer.length);
 
-                // 应用汉宁窗
-                applyHanningWindow(windowData);
+            // 应用汉宁窗进行预处理
+            applyHanningWindow(combinedBuffer_double);
 
-                // 进行FFT变换
-                double[] fftData = applyFFT(windowData);
+            // 利用滑动窗口移动，找出目标频率的位置
+            int targetIndex = findTargetIndex(combinedBuffer_double);
 
-                // 计算幅度最大的频率
-                int maxFrequencyIndex = findMaxFrequencyIndex(fftData);
-                double maxFrequency = calculateFrequency(maxFrequencyIndex);
-
-                // 判断是否接收到信号
-                if (Math.abs(maxFrequency - TARGET_FREQUENCY) <= MAX_FREQUENCY_DEVIATION) {
-                    // 记录时间
-                     RecordTime = System.currentTimeMillis();
-                    Log.d(TAG, "收到信号的时间: "+ RecordTime);
-                    // 处理信号 2177.2108.2124.2152.2127.2128.2117.2156.2084.2102.2081.2123.2167.2148.2119.2156.2157.2089.2134.2150///5516,5277,5225,5329,
-                    mTargetFrequencyDetected = true;
-                    break;
-                }
+            if (targetIndex != -1) {
+                // 根据时间上的等比关系求出第一次检测出目标的时间
+                long targetTime =  mPreviousStartTime + (targetIndex  / mSampleRate) * 1000;
+                // 处理目标时间
+                processTargetTime(targetTime);
             }
+
+            // 只保存当前处理数组的前一个数组
+            mPreviousBuffer = Arrays.copyOfRange(audioBuffer, 0, bytesRead);
+        } else {
+            // 未检测到目标频率，保存当前处理数组
+            mPreviousBuffer = Arrays.copyOfRange(audioBuffer, 0, bytesRead);
         }
-
-        if (mTargetFrequencyDetected) {
-            // 处理信号
-            mIsRecording = false;
-
-        }
-
     }
 
-    private double[] extractWindowData(short[] audioData, int startIndex) {
-        double[] windowData = new double[WINDOW_SIZE];
-        for (int i = 0; i < WINDOW_SIZE; i++) {
-            windowData[i] = audioData[startIndex + i];
+    private boolean detectTargetFrequency(short[] audioBuffer, int bytesRead) {
+
+        double[] audioData = convertToDoubleArray(audioBuffer, bytesRead);
+
+        // 应用汉宁窗进行预处理
+        applyHanningWindow(audioData);//4096
+
+        // 执行FFT变换
+
+        double[] fftData = applyFFT(audioData);
+
+        // 计算幅值最大的频率
+        int maxAmplitudeIndex = findMaxFrequencyIndex(fftData);
+        double maxAmplitudeFrequency = maxAmplitudeIndex * (mSampleRate / BUFFER_SIZE);
+//        Log.d(TAG, "1-幅值最大的频率: " + maxAmplitudeFrequency);
+
+
+
+        // 判断幅值最大的频率是否在浮动范围内
+        if (Math.abs(maxAmplitudeFrequency - targetFrequency) <= frequencyRange) {
+            // 第一次检测到目标频率，返回true
+            return true;
         }
-        return windowData;
+
+        // 未检测到目标频率，返回false
+        return false;
+    }
+
+
+    private short[] combineBuffers(short[] previousBuffer, short[] currentBuffer, int bytesRead) {
+        int combinedLength = previousBuffer.length + bytesRead;
+        short[] combinedBuffer = new short[combinedLength];
+        System.arraycopy(previousBuffer, 0, combinedBuffer, 0, previousBuffer.length);
+        System.arraycopy(currentBuffer, 0, combinedBuffer, previousBuffer.length, bytesRead);
+        return combinedBuffer;
+    }
+
+    private int findTargetIndex(double[] buffer) {
+
+
+        // 将拼接的两个数组进行滑动窗口遍历
+        for (int i = 0; i <= buffer.length - windowSize; i++) {
+            // 获取当前窗口的数据
+            double[] windowData = Arrays.copyOfRange(buffer, i, i + windowSize);
+            //窗口开始位置+目标频率在窗口内的位置
+            int targetIndex = i + findTargetFrequencyIndex(windowData, windowSize);
+            Log.d(TAG, "findTargetIndex: "+ targetIndex);
+            return targetIndex;
+        }
+
+        // 未找到目标频率，返回-1
+        return -1;
+    }
+
+
+    private void processTargetTime(long targetTime) {
+        // 处理目标时间
     }
 
     private void applyHanningWindow(double[] windowData) {
-        for (int i = 0; i < WINDOW_SIZE; i++) {
-            double multiplier = 0.5 * (1 - Math.cos(2 * Math.PI * i / (WINDOW_SIZE - 1)));
+        for (int i = 0; i < windowData.length; i++) {
+            double multiplier = 0.5 * (1 - Math.cos(2 * Math.PI * i / (windowData.length - 1)));
             windowData[i] *= multiplier;
         }
-    }
-
-    private double[] applyFFT(double[] windowData) {
-        DoubleFFT_1D fft = new DoubleFFT_1D(WINDOW_SIZE);
-        double[] fftData = new double[2 * WINDOW_SIZE];
-        System.arraycopy(windowData, 0, fftData, 0, WINDOW_SIZE);
-        fft.realForwardFull(fftData);
-        return fftData;
     }
 
     private int findMaxFrequencyIndex(double[] fftData) {
         int maxIndex = 0;
         double maxAmplitude = 0;
-        for (int i = 0; i < WINDOW_SIZE; i++) {
+        for (int i = 0; i < fftData.length/2; i++) {
             double amplitude = Math.sqrt(Math.pow(fftData[2 * i], 2) + Math.pow(fftData[2 * i + 1], 2));
             if (amplitude > maxAmplitude) {
                 maxAmplitude = amplitude;
                 maxIndex = i;
             }
         }
+//        double f = maxIndex*mSampleRate/(fftData.length);
+//        Log.d(TAG, "findMaxFrequencyIndex: " + f);
         return maxIndex;
     }
 
-    private double calculateFrequency(int index) {
-        return index * ((double) mSampleRate / WINDOW_SIZE);
+    private double[] convertToDoubleArray(short[] audioBuffer, int length) {
+        double[] doubleArray = new double[length];
+        for (int i = 0; i < length; i++) {
+            doubleArray[i] = (double) audioBuffer[i];
+        }
+        return doubleArray;
+    }
+
+    private double[] applyFFT(double[] windowData) {
+        DoubleFFT_1D fft = new DoubleFFT_1D(windowData.length);
+        double[] fftData = new double[2 * windowData.length];
+        System.arraycopy(windowData, 0, fftData, 0, windowData.length);
+        fft.realForwardFull(fftData);
+        return fftData;
+    }
+
+    private int findTargetFrequencyIndex(double[] windowData, int windowSize) {
+        // 将窗口数据进行 FFT 变换
+        double[] fftData = applyFFT(windowData);
+
+        // 计算频率分辨率
+        double frequencyResolution = mSampleRate / windowSize;
+
+
+        // 初始化最大幅度和对应的索引
+        double maxAmplitude = 0;
+        int maxAmplitudeIndex = -1;
+
+        // 在频域中查找幅度最大的频率
+        for (int i = 0; i < windowSize / 2; i++) {
+            double amplitude = Math.sqrt(fftData[2 * i] * fftData[2 * i] + fftData[2 * i + 1] * fftData[2 * i + 1]);
+            double frequency = i * frequencyResolution;
+            Log.d(TAG, "最大幅值的频率-2: " +  frequency);
+            // 判断频率是否在目标频率的上下浮动范围内
+            if (Math.abs(frequency-targetFrequency) <= frequencyRange && amplitude > maxAmplitude) {
+                maxAmplitude = amplitude;
+                maxAmplitudeIndex = i;
+            }
+        }
+
+        return maxAmplitudeIndex;
     }
 
 
+    public void stopRecording() {
+        mIsRecording = false;
+    }
 
 }
 
